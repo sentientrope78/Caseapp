4b4ab3076d59d87a9da07721e5e071c09112daae
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-13 19:30:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import './index.css';
 import App from './App';
+import { Router } from 'react-router-dom';
 
 // Create a root.
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // Initial render
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
