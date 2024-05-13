32b66477717c516c643f37f6a6b7b36f09a68b57
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-13 19:32:39 -0400

    Update Upload.js

--- Upload.js
+++ Upload.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Upload.css';
 
 function Upload() {
+  const handleClick = (event) => {
+    event.preventDefault()
+    window.location.href = "/results"
+  }
   return (
     <div className="Upload">
         <h1>Logged In</h1>
         <br></br>
         <h1>Upload Your File Below, you black monkey</h1>
-        <button>Upload</button>
+        <button onClick={handleClick}>Upload</button>
     </div>
   );
 }
