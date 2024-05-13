e5daa5c481250e879ec41d20b939de3673d21646
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-13 19:32:12 -0400

    Add files via upload

---
+++ Results.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Results.css';
+
+function Results() {
+  return (
+    <div className="Results">
+        <div class="row">
+            <div class="column Analysis">
+                <h1>Analysis:</h1>
+                <br></br> <br></br>
+                <h3>Patient is severely Black!</h3>
+            </div>
+            <div class="column Recommendations">
+                <h1>Recommendations:</h1>
+                <br></br> <br></br>
+                <h3>Best mode of treatment is Post-Birth Abortion!</h3>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Results;
