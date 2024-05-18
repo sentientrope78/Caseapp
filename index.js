e4b4f11aa4b08f1bc2345c7b2c2ac1e3e9892aa0
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-18 17:32:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Correct import for React 18
+import './index.css';
+import App from './App';
+import { Router } from 'react-router-dom';
+
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Initial render
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
