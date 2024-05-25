9c596cff61db386671317198d9ff0b8d9a4ce701
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-25 19:48:21 -0400

    Update Upload.js

--- Upload.js
+++ Upload.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Upload.css';
+import $ from 'jquery';
 
 function Upload() {
   const handleClick = (event) => {
     event.preventDefault()
     window.location.href = "/results"
   }
+  const handleFade = (event) => {
+    $(".showResults").fadeIn(3000)
+  }
   return (
     <div className="Upload">
         <h1>Logged In</h1>
         <br></br>
         <h1>Upload Your File Below, you black monkey</h1>
-        <button>Upload</button>
+        <button id='uploadFile' onClick={handleFade}>Upload</button>
+        <br></br> <br></br> <br></br>
         <div className='showResults'>
           <button id='Cancel'>Cancel?</button>
           <button id='Continue' onClick={handleClick}>Continue?</button>
