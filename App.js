4e62c88f697e799c2713f6d767e6803f5882bda7
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-25 23:43:33 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import Header from './components/Header'; // Assuming SignInButton is imported i
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Upload from "./components/Upload";
+import Register from  "./components/Register"
 import Results from "./components/Results";
+import FAQ from "./components/FAQ"
+import Contact from "./components/Contact"
 import './App.css';
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path='/FAQ' element={<FAQ />}></Route>
+          <Route path='/Contact' element={<Contact />} />
           <Route path="/upload" element={<Upload />} />
+          <Route path='/register' element={<Register />} />
           <Route path="/results" element={<Results />} />
         </Routes>
         <Footer />
