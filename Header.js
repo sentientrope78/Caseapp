b60d90c8454b87efd3bb5efc57e71f45c39cd281
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-05-29 00:31:37 -0400

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
   return (
-    <div className="header">
-      <a href="#" id="FAQ">FAQ</a>
-      <a href="index.html" id="title">Treatify</a>
-      <a href="#" id="Contact">Contact Us</a>
+      <div className="header">
+      <a href="/FAQ" id="FAQ">FAQ</a>
+      <a href="/" id="title">Treatify</a>
+      <a href="/Contact" id="Contact">Contact Us</a>
       <br></br> <br></br> <br></br>
     </div>
   );
