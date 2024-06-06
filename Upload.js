7feb95c86bc11c8977067c774a157fbe90b30b21
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-06-05 23:15:51 -0400

    Update Upload.js

--- Upload.js
+++ Upload.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Upload.css';
 import $ from 'jquery';
+// Import the main component
+import { Viewer } from '@react-pdf-viewer/core'; // install this library
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+// Worker
+import { Worker } from '@react-pdf-viewer/core'; // install this library
 
 function Upload() {
   const handleClick = (event) => {
@@ -10,19 +19,94 @@ function Upload() {
   const handleFade = (event) => {
     $(".showResults").fadeIn(3000)
   }
+  // Create new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
+  // for onchange event
+  const [pdfFile, setPdfFile]=useState(null);
+  const [pdfFileError, setPdfFileError]=useState('');
+
+  // for submit event
+  const [viewPdf, setViewPdf]=useState(null);
+
+  // onchange event
+  const fileType=['application/pdf'];
+  const handlePdfFileChange=(e)=>{
+    let selectedFile=e.target.files[0];
+    if(selectedFile){
+      if(selectedFile&&fileType.includes(selectedFile.type)){
+        let reader = new FileReader();
+            reader.readAsDataURL(selectedFile);
+            reader.onloadend = (e) =>{
+              setPdfFile(e.target.result);
+              setPdfFileError('');
+            }
+      }
+      else{
+        setPdfFile(null);
+        setPdfFileError('Please select valid pdf file');
+      }
+    }
+    else{
+      console.log('select your file');
+    }
+  }
+
+  // form submit
+  const handlePdfFileSubmit=(e)=>{
+    e.preventDefault();
+    if(pdfFile!==null){
+      setViewPdf(pdfFile);
+    }
+    else{
+      setViewPdf(null);
+    }
+  }
   return (
     <div className="Upload">
         <h1>Logged In</h1>
         <br></br>
         <h1>Upload Your File Below, you black monkey</h1>
-        <button id='uploadFile' onClick={handleFade}>Upload</button>
+  
+
+    <div className='container'>
+
+    <br></br>
+    
+      <form className='form-group' onSubmit={handlePdfFileSubmit}>
+        <input type="file" className='form-control'
+          required onChange={(e) => { handlePdfFileChange(e); handlePdfFileSubmit(e);}}
+        />
+        {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
+        <br></br>
+        <button type="submit" id='uploadFile' onClick={handleFade}>
+          UPLOAD
+        </button>
+      </form>
+      <br></br>
+      <h4>View PDF</h4>
+      <div className='pdf-container'>
+        {/* show pdf conditionally (if we have one)  */}
+        {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+          <Viewer fileUrl={viewPdf}
+            plugins={[defaultLayoutPluginInstance]} />
+      </Worker></>}
+
+      {/* if we dont have pdf or viewPdf state is null */}
+      {!viewPdf&&<>No pdf file selected</>}
+      </div>
+
+    </div>
+
         <br></br> <br></br> <br></br>
         <div className='showResults'>
           <button id='Cancel'>Cancel?</button>
           <button id='Continue' onClick={handleClick}>Continue?</button>
         </div>
     </div>
+    
   );
+  
 }
 
 export default Upload;
