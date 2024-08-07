b96cb14bd778f9e11150935cc8d50effb90774b4
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-08-06 22:14:57 -0400

    Update Results.js with Improved Treatment and GPT Analysis

--- Results.js
+++ Results.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Results.css';
+import Popup from './Loading.js';
+import axios from 'axios';
 
 function Results() {
+  const [analysis, setAnalysis] = useState('');
+  const [text, setText] = useState('')
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const analyzeText = async () => {
+    setIsPopupOpen(true);
+    try {
+      const response = await fetch('http://localhost:8008/analyze', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+      const data = await response.json();
+      setAnalysis(data.analysis || 'No analysis available');
+      console.log(data.analysis);
+      
+    } catch (error) {
+      console.error('Error analyzing text:', error);
+      setAnalysis('Failed to analyze text');
+    }
+    setIsPopupOpen(false);
+  };
+
+  useEffect(() => {
+    receiveText()
+    analyzeText()
+  }, [])
+
+  const receiveText = async() => {
+    const response = await axios.get('http://localhost:4007/view-text');
+    const extractedText = response.data;
+    setText(extractedText);
+    console.log(text);
+  }
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+  
+
   return (
     <div className="Results">
+      {isPopupOpen && <Popup />}
         <div class="row">
-            <div class="column Analysis">
+            <div className="column Analysis">
                 <h1>Analysis:</h1>
+                <p>{text}</p>
                 <br></br> <br></br>
-                <h3>Patient is severely Black!</h3>
             </div>
-            <div class="column Recommendations">
+            <div className="column Recommendations">
                 <h1>Recommendations:</h1>
                 <br></br> <br></br>
-                <h3>Best mode of treatment is Post-Birth Abortion!</h3>
+                <p>{analysis}</p>
             </div>
         </div>
     </div>
