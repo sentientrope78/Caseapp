e6951d561b9371cc17be02a883bfafd99d7ddb32
AmitRiya14 <64803346+AmitRiya14@users.noreply.github.com>
2024-08-08 14:47:55 -0400

    Update indexBackend.js which runs all other backend files too

--- indexBackend.js
+++ indexBackend.js
@@ -3,6 +3,10 @@ import mysql from "mysql"
 import fs from "fs"
 import path from "path"
 import cors from "cors"
+import './Analyzer.mjs'
+import './ExtractedText.mjs'
+import './data.mjs'
+import './openai.mjs'
 
 const app = express();
 const port = 3001;
@@ -19,7 +23,7 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(port, () => {
-  console.log("Server running at http://localhost:${port}");
+  console.log(`Index server is running on port: ${port}`);
 });
 
 app.get("/", (req,res) => {
